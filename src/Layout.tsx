0785c69010aa43ea97dadf5c61a11bf9d8b6c3e3
ravikmr0 <mbravikumar0@gmail.com>
2026-08-31 16:22:37 +0530

    update

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -140,7 +140,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className="container">
           <div className="footer-top">
             <div className="footer-wordmark-block">
-              <h4 className="footer-col-title footer-profile-title">Profile</h4>
               <div className="footer-wordmark-shell">
                 <img className="brand-image" src="/estatement_logo.png" alt="Estatement" />
               </div>
